3f4730b6f435017e1acc058468a1f56a8eb8e6b8
Guido <molido@hotmail.it>
2020-06-15 15:31:50 +0200

    Primo Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+
+// numero di km da percorrere
+var km = prompt("Ciao! Quanti km vuoi percorrere?");
+// età passeggero
